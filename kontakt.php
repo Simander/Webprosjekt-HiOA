47fde1eb17ee5ee3fc75ba2640549e5437836aed
silju <bringsli@live.no>
2013-10-30 15:55:31 +0100

    Update kontakt.php

---
+++ kontakt.php
@@ -0,0 +1,41 @@
+<?php
+
+<h2> Registrer deg som kunde hos oss!</h2>
+
+<form action="kontaktsvar.php" method="get">
+
+Navn: <br/>
+<input type="text" name="navn"/><br/>
+
+Adresse: <br/>
+<input type="text" name="adresse"/><br/>
+
+Telefon: <br/>
+<input type="text" name="telefon"/><br/>
+
+Mail: <br/>
+<input type="text" name="mail"/><br/>
+</select><br/>
+
+
+Ønsker du å få tilbakemelding per telefon eller email?
+
+<input type="radio" name="tilbake" value="Via telefon"/><br/><br/>
+
+<input type="radio" name?"tilbake" value="Via mail"/><br/><br/>
+
+
+<form method="post" action="<br/> <br/>">
+
+<textarea rows="5" cols="20" name="svar"> Hva lurer du på?</textarea>
+
+<input type="submit" value="Send inn" name="knapp"/>
+
+</form>
+
+
+
+
+
+
+?>
