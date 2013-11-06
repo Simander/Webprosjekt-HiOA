25d038d1953b8533969042f8ef2d951b6760fdab
JoakimT <joakimtommer@gmail.com>
2013-11-06 14:54:44 +0100

    admin opprettet
    pseudo kode something

---
+++ admin.php
@@ -0,0 +1,27 @@
+<!-- Login form
+
+
+1 bruker - admin
+1 passord - sett passord til en rar "sikker" kombinasjon av taster, tall og bokstaver.
+
+
+hvis riktig login -> åpne for redigering av kalender
+hvis feil login -> vise feilmelding og gjøre et nytt forsåk på logon
+
+(hvis forsøk på 3 logins i rask rekkefølge, sperre mulighet for login i et set antall minutter) <- sikkerhets messig
+
+
+Side med mulighet for å redigere true eller false per time på kalender.html
+^^ 
+
+
+
+　   ∧,_,∧
+　  (`･ω･´)　 ｎ_
+ η  ＞　 ⌒＼/､_∃
+(∃) /   ∧　　＼_/
+ ＼_/　＼　　丶
+ 　　　／ y　 ﾉ
+ 　　／ ／　／
+　 ( (　(　〈
+　　＼ ＼  ＼
