b6606f39526a7e1f133ffcc139f3507d069e5e2f
Joakim Tømmer <joakimtommer@gmail.com>
2013-11-09 13:38:37 +0100

    Løkkenissen

--- kalender.js
+++ kalender.js
@@ -1,23 +1,5 @@
-	var hour = 0;
-	var hourstotal = 24;
-	var daycount = 0;
-	
-	for (var i = 0; <= hourstotal; i++)
-	{
-		hour++;
-		if ( hour == hourstotal)
-		{
-			for (var i = 0; i <= dager.length; i++)
-			{	
-				dager[i++];
-				daycount++
-				
-			}
-		}
-		
-	}
-	
-	
+
+
 	var dager = new array();
 		dager[1] = "Mandag";
 		dager[2] = "Tirsdag";
@@ -42,7 +24,39 @@
 		måned[11] = "November";
 		måned[12] = "December";
 	
-	var year = 2014;
+	//var year = 2014;
+	//var år = 2000;		
+	var hour = 0; // Time teller
+	var hourstotal = 24; // max antall timer på et døgn
+	var daycount = 0; // Teller for dager
+	var daystotal = get.date?; // Hente totale dager i måneden
+	var year = 2014; // Hente ut år
 	
-
-	var år = 2000;		
+	for (var i = 0; <= hourstotal; i++) // Løkkeception -_-
+	{
+		hour++;  // Øker timeteller med 1
+		if ( hour == hourstotal) //instruks for løkke som går til max antall timer på et døgn
+		{
+			for (var j = 0; j <= dager.length; j++)
+			{	
+				dager[j++]; // øker dag array med 1, Altså navnet på dagen.
+				daycount++ // Dag teller øker med 1
+				hour = 0; // reset timer
+				
+				if ( daycount == 30) // om dag teller når slutten av måneden <- må sette inn riktig date get metode
+				{
+					for ( int k = 0; k <= måned.length; k++)
+					{
+						måned[k++]; // øker måned med 1
+						daycount = 0; // reset dager
+					}	
+				}		
+					
+				
+			}
+		}
+		
+	}
+	
+	
+	
