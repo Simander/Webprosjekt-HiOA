3ff9f3de88a9ce3ae5d9b02d6f6ba8e44038a945
Joakim Tømmer <joakimtommer@gmail.com>
2013-11-09 13:12:06 +0100

    kalender morro

---
+++ kalender.js
@@ -0,0 +1,48 @@
+	var hour = 0;
+	var hourstotal = 24;
+	var daycount = 0;
+	
+	for (var i = 0; <= hourstotal; i++)
+	{
+		hour++;
+		if ( hour == hourstotal)
+		{
+			for (var i = 0; i <= dager.length; i++)
+			{	
+				dager[i++];
+				daycount++
+				
+			}
+		}
+		
+	}
+	
+	
+	var dager = new array();
+		dager[1] = "Mandag";
+		dager[2] = "Tirsdag";
+		dager[3] = "Onsdag";
+		dager[4] = "Torsdag";
+		dager[5] = "Fredag";
+		dager[6] = "Lørdag";
+		dager[7] = "Søndag";
+		
+	
+	var måneder = new array();
+		måned[1] = "Januar";
+		måned[2] = "February";
+		måned[3] = "March";
+		måned[4] = "April";
+		måned[5] = "May";
+		måned[6] = "June";
+		måned[7] = "July";
+		måned[8] = "August";
+		måned[9] = "September";
+		måned[10] = "October";
+		måned[11] = "November";
+		måned[12] = "December";
+	
+	var year = 2014;
+	
+
+	var år = 2000;		
