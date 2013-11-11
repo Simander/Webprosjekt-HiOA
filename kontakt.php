634d4ad15dbec89099996875eb5d87cfba6bf77b
Anders Simonsen <anders.stende.simonsen@gmail.com>
2013-11-11 16:05:59 +0100

    easter egg implementering

--- kontakt.php
+++ kontakt.php
@@ -1,16 +1,21 @@
 
-
-
-<form action="mailform.php" method="post">
-<p id="e-post_navn">
-Navn:<br /><input type="text" name="navn" size="20" maxlength="50" tabindex="1" />
-</p>
+<script src="/easter_implementation.js"></script>
+<div id="kontakt_skjema">
+ <form action="mailform.php" method="post">
+ <p class="mail_form" id="e-post_navn">
+ Navn:<br /><input type="text" name="navn" size="20" maxlength="50" tabindex="1" />
+ </p>
  
-<p id="e-post_epost">
-E-post:<br /><input type="text" name="epost" size="20" maxlength="50" tabindex="2" />
-</p>
+ <p class="mail_form" id="e-post_epost">
+ E-post:<br /><input type="text" name="epost" size="20" maxlength="50" tabindex="2" />
+ </p>
+</div>
+<div id="hidden_shell">
+ <img id="shell_img" width="240" height="150" src="mixer.jpg">
+ <div id="easter_object"></div>
+</div>
  
-<p id="e-post_melding">Melding:<br />
+<p class="mail_form" id="e-post_melding">Melding:<br />
 <textarea cols="72" rows="10" name="tekst" tabindex="3" class="e-post_melding">
 </textarea><br />
  
