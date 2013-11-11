35cd0f7d3fd6c1bfe04987be84bd71e9a7ff14fe
Anders Simonsen <anders.stende.simonsen@gmail.com>
2013-11-11 16:06:51 +0100

    easter egg implementering

---
+++ easter_implementation.js
@@ -0,0 +1,7 @@
+				$(document).ready(function(){
+				//easter hidden egg
+		   		$("#easter_object").click(function(){
+		    		$("#false").hide("slow");
+		    		window.location.href= "index.php?page=5";	    
+		  		});
+		  		});
