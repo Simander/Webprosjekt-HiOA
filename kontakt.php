1aa714236a3166f33ad9df2ffae7888467ac097f
Anders Simonsen <anders.stende.simonsen@gmail.com>
2013-11-11 16:12:40 +0100

    Update kontakt.php

--- kontakt.php
+++ kontakt.php
@@ -1,29 +1,29 @@
-
 <script src="/easter_implementation.js"></script>
 <div id="kontakt_skjema">
- <form action="mailform.php" method="post">
- <p class="mail_form" id="e-post_navn">
- Navn:<br /><input type="text" name="navn" size="20" maxlength="50" tabindex="1" />
- </p>
+  <form action="mailform.php" method="post">
+  
+  <p class="mail_form" id="e-post_navn">
+  Navn:<br /><input type="text" name="navn" size="20" maxlength="50" tabindex="1" />
+  </p>
  
- <p class="mail_form" id="e-post_epost">
- E-post:<br /><input type="text" name="epost" size="20" maxlength="50" tabindex="2" />
- </p>
-</div>
+  <p class="mail_form" id="e-post_epost">
+  E-post:<br /><input type="text" name="epost" size="20" maxlength="50" tabindex="2" />
+  </p>
+
 <div id="hidden_shell">
  <img id="shell_img" width="240" height="150" src="mixer.jpg">
  <div id="easter_object"></div>
 </div>
  
-<p class="mail_form" id="e-post_melding">Melding:<br />
-<textarea cols="72" rows="10" name="tekst" tabindex="3" class="e-post_melding">
-</textarea><br />
- 
-<input type="submit" name="submit" value="Send" tabindex="4" class="epost_knapp" />
-<input type="reset" name="reset" value="Slett" tabindex="5" class="epost_knapp" />
+ <p class="mail_form" id="e-post_melding">Melding:<br />
+ <textarea cols="72" rows="10" name="tekst" tabindex="3" class="e-post_melding">
+ </textarea><br />
+  
+ <input type="submit" name="submit" value="Send" tabindex="4" class="epost_knapp" />
+ <input type="reset" name="reset" value="Slett" tabindex="5" class="epost_knapp" />
  
 </form>
-
+</div>
 
 
 
