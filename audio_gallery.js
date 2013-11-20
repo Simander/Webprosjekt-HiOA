89ae74f0b5fc7e1440fd42b4c7b341adf819a3ef
Anders Simonsen <anders.stende.simonsen@gmail.com>
2013-11-20 17:51:35 +0100

    fix

--- audio_gallery.js
+++ audio_gallery.js
@@ -6,13 +6,18 @@ $(document).ready(function(){
 			$("#audio_samples").toggle("fast");
 			$("#sound").toggle();
 			$("#skjul").toggle();
+			 var knapp1=$("#sound");  
+    				knapp1.animate({left:'29em'},"slow");
+    			var knapp2=$("#skjul");  
+    				knapp2.animate({left:'29em'},"slow");
 		}); 
 		$("#skjul").click(function(){
 			$("#audio_samples").toggle("fast");
 			$("#skjul").toggle();
 			$("#sound").toggle();
-			
-			
+			 var knapp1=$("#sound");  
+    				knapp1.animate({left:'1em'},"slow");
+    			var knapp2=$("#skjul");  
+    				knapp2.animate({left:'1em'},"slow");		
 		});	
 });
-	
