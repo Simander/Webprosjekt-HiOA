39dbaf60f8d86fc47436af0755aa97591f5a33c4
Anders Simonsen <anders.stende.simonsen@gmail.com>
2013-11-22 20:25:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,14 @@ if(!empty($_GET)){
                 <title>Studio Flagolett</title>
         </head>
         <body>
+			<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+  				var js, fjs = d.getElementsByTagName(s)[0];
+				  if (d.getElementById(id)) return;
+				  js = d.createElement(s); js.id = id;
+				  js.src = "//connect.facebook.net/nn_NO/all.js#xfbml=1";
+  					fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));</script>
                 <div id="centre">
                         <section id="top_bar">
                                 <header>
@@ -81,8 +89,10 @@ if(!empty($_GET)){
                         <br>
                         <section id="bottom_bar">
                                 <footer>
-                                        <p>Copyright: Studio Flagolett</p>
-                                        <p>kontakt.flagolett@gmail.com</p>
+                                        <p class="footy"><strong>	
+						Copyright: Studio Flagolett
+						<br>kontakt.flagolett@gmail.com<strong></p>
+					<div class="fb-like" data-href="https://www.facebook.com/flagoletten" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
                                 </footer>
                         </section>
                 </div>
