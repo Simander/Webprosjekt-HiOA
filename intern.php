4b03c712edb5eaa2dc571b64b4e615005f92fe22
silju <bringsli@live.no>
2013-11-22 19:42:28 +0100

    Update intern.php

--- intern.php
+++ intern.php
@@ -1,5 +1,4 @@
 
-
 <?php
 error_reporting(E_ALL);
 
@@ -8,7 +7,10 @@ session_start();
 //@session_start(); //@ gjør at feilmelding ikke vises
 if (isset($_SESSION['innlogget'])) {
 
-
+	
+	echo "<p> Da har du logget inn på vår hemmelige side" . $_SESSION['navn'] . "</p>";
+}
+	
 	 include "internkladd.php"; 
 
 }
