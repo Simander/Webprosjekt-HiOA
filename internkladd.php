1b65d718a9c0d667c41f61a2f62b5a7207fc7526
Anders Simonsen <anders.stende.simonsen@gmail.com>
2013-11-24 17:03:16 +0100

    Update internkladd.php

--- internkladd.php
+++ internkladd.php
@@ -5,49 +5,51 @@ if(!empty($_GET)){
 	$page = "";
 }
 ?>
-
-<head>
-	<link rel="stylesheet" title="layout1" href="adminstil.css">
-	<meta charset="UTF-8">
-	<title>Admin</title>
-</head>
-<body>
-	<div id="adminframe"> <!-- Hovedvindu-->
-		<nav id="navigasjon"> <!-- Navigasjonsbar-->
-		<!-- Epost knapp, access login gmail-->
-				<a class="button" id="mail" href="internkladd.php?page=1">
-					E-post
-				</a>
-
-				<a class="button" id="soundcloud" href="internkladd.php?page=2"> <!-- Soundcloud admin knapp, access login soundcloud-->
-					Soundcloud Admin
-				</a>
-		
-				<a class="button" id="kalender" href="internkladd.php?page=3"> <!-- trykkbar versjon av kalender html, endringer skjer her-->
-					Kalender Admin
-				</a>
-		</nav>
-		<section id="include"> <!-- vindu hvor sidene blir inkludert-->
-			<?php
-				switch($page)
-				{
-				 case 1:
-					include('epost.html');
-				break;
-						
-				case 2:
-					include('soundcloud.html');
-				break;
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link rel="stylesheet" title="layout1" href="adminstil.css">
+		<meta charset="UTF-8">
+		<title>Admin</title>
+	</head>
+	<body>
+		<div id="adminframe"> <!-- Hovedvindu-->
+			<nav id="navigasjon"> <!-- Navigasjonsbar-->
+			<!-- Epost knapp, access login gmail-->
+					<a class="button" id="mail" href="internkladd.php?page=1">
+						E-post
+					</a>
+	
+					<a class="button" id="soundcloud" href="internkladd.php?page=2"> <!-- Soundcloud admin knapp, access login soundcloud-->
+						Soundcloud Admin
+					</a>
+			
+					<a class="button" id="kalender" href="internkladd.php?page=3"> <!-- trykkbar versjon av kalender html, endringer skjer her-->
+						Kalender Admin
+					</a>
+			</nav>
+			<section id="include"> <!-- vindu hvor sidene blir inkludert-->
+				<?php
+					switch($page)
+					{
+					 case 1:
+						include('epost.html');
+					break;
+							
+					case 2:
+						include('soundcloud.html');
+					break;
 				
-				case 3: 
-					include('kalenderinfo.html');
+					case 3: 
+						include('kalenderinfo.html');
 						
-				break;
+					break;
 				
-				default:
-					include('nothing.html');
-				}
-			?>			
+					default:
+						include('nothing.html');
+					}
+				?>			
 		</section>
 	</div>
-</body>
+	</body>
+</html>
