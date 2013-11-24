32868d2d9368b5f35478adbaa9e7901c59bd20a1
Anders Simonsen <anders.stende.simonsen@gmail.com>
2013-11-24 17:53:00 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,6 +2,6 @@ var kkeys = [], admin = "65,68,77,73,78";
 	$(document).keydown(function(e) {
 	kkeys.push( e.keyCode );
 	if ( kkeys.toString().indexOf( admin ) >=0 ){
-		window.location.href= "intern.php";
+		window.location.href= "2E4A967CF0.php";
 		};
 	});
