a5c048d581d0b1b4ea0fe0321e7d63c2d3a36dde
silju <bringsli@live.no>
2013-11-24 21:07:26 +0100

    ..

--- intern.php
+++ intern.php
@@ -1,6 +1,6 @@
 
 <?php
-error_reporting(E_ALL);
+
 
 
 session_start();
@@ -10,15 +10,14 @@ if (isset($_SESSION['innlogget'])) {
 	
 	echo "<p> Da har du logget inn på vår hemmelige side" . $_SESSION['navn'] . "</p>";
 
-	
-	 include "internkladd.php"; 
 
 }
 
+
 	else {
+
 
 		include "login.php"; //skjema for innlogging
 	}
 
 ?>
-
