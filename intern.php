47b3846bec132aabadea63c8aa31c5830138d7da
JoakimT <joakimtommer@gmail.com>
2013-11-24 16:16:23 +0100

    endringer

--- intern.php
+++ intern.php
@@ -4,12 +4,12 @@ error_reporting(E_ALL);
 
 
 session_start();
-//@session_start(); //@ gjør at feilmelding ikke vises
+
 if (isset($_SESSION['innlogget'])) {
 
 	
 	echo "<p> Da har du logget inn på vår hemmelige side" . $_SESSION['navn'] . "</p>";
-}
+
 	
 	 include "internkladd.php"; 
 
